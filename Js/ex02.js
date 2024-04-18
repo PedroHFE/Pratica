74d48df9da4ba643ae184472649e1f034a79955c
PedroHFE <pdro.hf@gmail.com>
2024-04-17 23:10:58 -0300

    ex02

---
+++ Js/ex02.js
@@ -0,0 +1,7 @@
+function verificar(){
+    var data = new Date()
+    var idade = document.getElementById('anonasc')
+    var sexo = document.querySelector("input#sexom" || "input#sexof")
+    var imagem = document.querySelector("img#foto")
+    
+}
