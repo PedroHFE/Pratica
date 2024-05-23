bd3b5d3b4d527bb6b0c7311aa12283a95b144d42
PedroHFE <pdro.hf@gmail.com>
2024-05-23 00:16:41 -0300

    Finalização do ver mais + criação de osol e armaçoes

---
+++ teste/button.js
@@ -0,0 +1,60 @@
+//Botões da sessão Masculina
+function masculino1(vmm1){
+    document.getElementById(vmm1).style.display = 'block';
+    document.querySelector('button#bmvm1').style.display = 'none';
+    document.querySelector('button#bmvm2').style.display = 'block';
+}
+function masculino2(vmm2){
+    document.getElementById(vmm2).style.display = 'block';
+    document.querySelector('button#bmvm2').style.display = 'none';
+    document.querySelector('button#bmvm3').style.display = 'block';
+}
+function masculino3(vmm3){
+    document.getElementById(vmm3).style.display = 'block';
+    document.querySelector('button#bmvm3').style.display = 'none'
+}
+//Botões da sessão Feminina
+function feminino1(vmf1){
+    document.getElementById(vmf1).style.display = 'block';
+    document.querySelector('button#bfvm1').style.display = 'none';
+    document.querySelector('button#bfvm2').style.display = 'block';
+}
+function feminino2(vmf2){
+    document.getElementById(vmf2).style.display = 'block';
+    document.querySelector('button#bfvm2').style.display = 'none';
+    document.querySelector('button#bfvm3').style.display = 'block';
+}
+function feminino3(vmf3){
+    document.getElementById(vmf3).style.display = 'block';
+    document.querySelector('button#bfvm3').style.display = 'none';
+}
+//Botões da sessão Infantil
+function infantil1(vmi1){
+    document.getElementById(vmi1).style.display = 'block';
+    document.querySelector('button#bivm1').style.display = 'none';
+    document.querySelector('button#bivm2').style.display = 'block';
+}
+function infantil2(vmi2){
+    document.getElementById(vmi2).style.display = 'block';
+    document.querySelector('button#bivm2').style.display = 'none';
+    document.querySelector('button#bivm3').style.display = 'block';
+}
+function infantil3(vmi3){
+    document.getElementById(vmi3).style.display = 'block';
+    document.querySelector('button#bivm3').style.display = 'none';
+}
+//Botões da sessão Unissex
+function unissex1(vmu1){
+    document.getElementById(vmu1).style.display = 'block';
+    document.querySelector('button#buvm1').style.display = 'none';
+    document.querySelector('button#buvm2').style.display = 'block';
+}
+function unissex2(vmu2){
+    document.getElementById(vmu2).style.display = 'block';
+    document.querySelector('button#buvm2').style.display = 'none';
+    document.querySelector('button#buvm3').style.display = 'block';
+}
+function unissex3(vmu3){
+    document.getElementById(vmu3).style.display = 'block';
+    document.querySelector('button#buvm3').style.display = 'none';
+}
