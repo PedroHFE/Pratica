d627340de085b9d130ea39683784eb1c3cf82470
PedroHFE <pdro.hf@gmail.com>
2024-05-30 00:43:35 -0300

    Finalizando botões de ver mais

--- teste/buttonsm.js
+++ teste/buttonsm.js
@@ -58,3 +58,123 @@ function gucci3(gu3){
     document.getElementById(gu3).style.display = 'block';
     document.querySelector('button#bgc3').style.display = 'none';
 }
+//Botões da sessão Persol
+function persol1(per1){
+    document.getElementById(per1).style.display = 'block';
+    document.querySelector('button#bpr1').style.display = 'none';
+    document.querySelector('button#bpr2').style.display = 'block';
+}
+function persol2(per2){
+    document.getElementById(per2).style.display = 'block';
+    document.querySelector('button#bpr2').style.display = 'none';
+    document.querySelector('button#bpr3').style.display = 'block';
+}
+function persol3(per3){
+    document.getElementById(per3).style.display = 'block';
+    document.querySelector('button#bpr3').style.display = 'none';
+}
+//Botões da sessão Dior
+function dior1(di1){
+    document.getElementById(di1).style.display = 'block';
+    document.querySelector('button#bdr1').style.display = 'none';
+    document.querySelector('button#bdr2').style.display = 'block';
+}
+function dior2(di2){
+    document.getElementById(di2).style.display = 'block';
+    document.querySelector('button#bdr2').style.display = 'none';
+    document.querySelector('button#bdr3').style.display = 'block';
+}
+function dior3(di3){
+    document.getElementById(di3).style.display = 'block';
+    document.querySelector('button#bdr3').style.display = 'none';
+}
+//Botões da sessão Carerra
+function carerra1(car1){
+    document.getElementById(car1).style.display = 'block';
+    document.querySelector('button#bcr1').style.display = 'none';
+    document.querySelector('button#bcr2').style.display = 'block';
+}
+function carerra2(car2){
+    document.getElementById(car2).style.display = 'block';
+    document.querySelector('button#bcr2').style.display = 'none';
+    document.querySelector('button#bcr3').style.display = 'block';
+}
+function carerra3(car3){
+    document.getElementById(car3).style.display = 'block';
+    document.querySelector('button#bcr3').style.display = 'none';
+}
+//Botões da sessão Vogue
+function vogue1(vo1){
+    document.getElementById(vo1).style.display = 'block';
+    document.querySelector('button#bvg1').style.display = 'none';
+    document.querySelector('button#bvg2').style.display = 'block';
+}
+function vogue2(vo2){
+    document.getElementById(vo2).style.display = 'block';
+    document.querySelector('button#bvg2').style.display = 'none';
+    document.querySelector('button#bvg3').style.display = 'block';
+}
+function vogue3(vo3){
+    document.getElementById(vo3).style.display = 'block';
+    document.querySelector('button#bvg3').style.display = 'none';
+}
+//Botões da sessão Ana Hickman
+function anahickman1(ana1){
+    document.getElementById(ana1).style.display = 'block';
+    document.querySelector('button#bah1').style.display = 'none';
+    document.querySelector('button#bah2').style.display = 'block';
+}
+function anahickman2(ana2){
+    document.getElementById(ana2).style.display = 'block';
+    document.querySelector('button#bah2').style.display = 'none';
+    document.querySelector('button#bah3').style.display = 'block';
+}
+function anahickman3(ana3){
+    document.getElementById(ana3).style.display = 'block';
+    document.querySelector('button#bah3').style.display = 'none';
+}
+//Botões da sessão Prada
+function prada1(pd1){
+    document.getElementById(pd1).style.display = 'block';
+    document.querySelector('button#bpd1').style.display = 'none';
+    document.querySelector('button#bpd2').style.display = 'block';
+}
+function prada2(pd2){
+    document.getElementById(pd2).style.display = 'block';
+    document.querySelector('button#bpd2').style.display = 'none';
+    document.querySelector('button#bpd3').style.display = 'block';
+}
+function prada3(pd3){
+    document.getElementById(pd3).style.display = 'block';
+    document.querySelector('button#bpd3').style.display = 'none';
+}
+//Botões da sessão Mormaii
+function mormaii1(mm1){
+    document.getElementById(mm1).style.display = 'block';
+    document.querySelector('button#bmm1').style.display = 'none';
+    document.querySelector('button#bmm2').style.display = 'block';
+}
+function mormaii2(mm2){
+    document.getElementById(mm2).style.display = 'block';
+    document.querySelector('button#bmm2').style.display = 'none';
+    document.querySelector('button#bmm3').style.display = 'block';
+}
+function mormaii3(mm3){
+    document.getElementById(mm3).style.display = 'block';
+    document.querySelector('button#bmm3').style.display = 'none';
+}
+//Botões da sessão Tom Ford
+function tomford1(tom1){
+    document.getElementById(tom1).style.display = 'block';
+    document.querySelector('button#btf1').style.display = 'none';
+    document.querySelector('button#btf2').style.display = 'block';
+}
+function tomford2(tom2){
+    document.getElementById(tom2).style.display = 'block';
+    document.querySelector('button#btf2').style.display = 'none';
+    document.querySelector('button#btf3').style.display = 'block';
+}
+function tomford3(tom3){
+    document.getElementById(tom3).style.display = 'block';
+    document.querySelector('button#btf3').style.display = 'none';
+}
