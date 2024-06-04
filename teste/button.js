056292dde3c75e54c69c28e241956f705aecbb90
PedroHFE <pdro.hf@gmail.com>
2024-06-04 13:23:27 -0300

    Carrunho icone e links produtos

--- teste/button.js
+++ teste/button.js
@@ -1,60 +1,195 @@
-//Botões da sessão Masculina
-function masculino1(vmm1){
-    document.getElementById(vmm1).style.display = 'block';
+//Botões da sessão Masculino
+function masculino1(masc1){
+    document.getElementById(masc1).style.display = 'block';
     document.querySelector('button#bmvm1').style.display = 'none';
     document.querySelector('button#bmvm2').style.display = 'block';
 }
-function masculino2(vmm2){
-    document.getElementById(vmm2).style.display = 'block';
+function masculino2(masc2){
+    document.getElementById(masc2).style.display = 'block';
     document.querySelector('button#bmvm2').style.display = 'none';
     document.querySelector('button#bmvm3').style.display = 'block';
 }
-function masculino3(vmm3){
-    document.getElementById(vmm3).style.display = 'block';
-    document.querySelector('button#bmvm3').style.display = 'none'
+function masculino3(masc3){
+    document.getElementById(masc3).style.display = 'block';
+    document.querySelector('button#bmvm3').style.display = 'none';
 }
-//Botões da sessão Feminina
-function feminino1(vmf1){
-    document.getElementById(vmf1).style.display = 'block';
+//Botões da sessão Feminino
+function feminino1(fem1){
+    document.getElementById(fem1).style.display = 'block';
     document.querySelector('button#bfvm1').style.display = 'none';
     document.querySelector('button#bfvm2').style.display = 'block';
 }
-function feminino2(vmf2){
-    document.getElementById(vmf2).style.display = 'block';
+function feminino2(fem2){
+    document.getElementById(fem2).style.display = 'block';
     document.querySelector('button#bfvm2').style.display = 'none';
     document.querySelector('button#bfvm3').style.display = 'block';
 }
-function feminino3(vmf3){
-    document.getElementById(vmf3).style.display = 'block';
+function feminino3(fem3){
+    document.getElementById(fem3).style.display = 'block';
     document.querySelector('button#bfvm3').style.display = 'none';
 }
 //Botões da sessão Infantil
-function infantil1(vmi1){
-    document.getElementById(vmi1).style.display = 'block';
+function infantil1(inf1){
+    document.getElementById(inf1).style.display = 'block';
     document.querySelector('button#bivm1').style.display = 'none';
     document.querySelector('button#bivm2').style.display = 'block';
 }
-function infantil2(vmi2){
-    document.getElementById(vmi2).style.display = 'block';
+function infantil2(inf2){
+    document.getElementById(inf2).style.display = 'block';
     document.querySelector('button#bivm2').style.display = 'none';
     document.querySelector('button#bivm3').style.display = 'block';
 }
-function infantil3(vmi3){
-    document.getElementById(vmi3).style.display = 'block';
+function infantil3(inf3){
+    document.getElementById(inf3).style.display = 'block';
     document.querySelector('button#bivm3').style.display = 'none';
 }
 //Botões da sessão Unissex
-function unissex1(vmu1){
-    document.getElementById(vmu1).style.display = 'block';
+function unissex1(uni1){
+    document.getElementById(uni1).style.display = 'block';
     document.querySelector('button#buvm1').style.display = 'none';
     document.querySelector('button#buvm2').style.display = 'block';
 }
-function unissex2(vmu2){
-    document.getElementById(vmu2).style.display = 'block';
+function unissex2(uni2){
+    document.getElementById(uni2).style.display = 'block';
     document.querySelector('button#buvm2').style.display = 'none';
     document.querySelector('button#buvm3').style.display = 'block';
 }
-function unissex3(vmu3){
-    document.getElementById(vmu3).style.display = 'block';
+function unissex3(uni3){
+    document.getElementById(uni3).style.display = 'block';
     document.querySelector('button#buvm3').style.display = 'none';
+}
+//Botões da sessão Retangular
+function retangular1(retan1){
+    document.getElementById(retan1).style.display = 'block';
+    document.querySelector('button#brt1').style.display = 'none';
+    document.querySelector('button#brt2').style.display = 'block';
+}
+function retangular2(retan2){
+    document.getElementById(retan2).style.display = 'block';
+    document.querySelector('button#brt2').style.display = 'none';
+    document.querySelector('button#brt3').style.display = 'block';
+}
+function retangular3(retan3){
+    document.getElementById(retan3).style.display = 'block';
+    document.querySelector('button#brt3').style.display = 'none'
+}
+//Botões da sessão Redondo
+function redondo1(red1){
+    document.getElementById(red1).style.display = 'block';
+    document.querySelector('button#brd1').style.display = 'none';
+    document.querySelector('button#brd2').style.display = 'block';
+}
+function redondo2(red2){
+    document.getElementById(red2).style.display = 'block';
+    document.querySelector('button#brd2').style.display = 'none';
+    document.querySelector('button#brd3').style.display = 'block';
+}
+function redondo3(red3){
+    document.getElementById(red3).style.display = 'block';
+    document.querySelector('button#brd3').style.display = 'none';
+}
+//Botões da sessão Quadrado
+function quadrado1(qua1){
+    document.getElementById(qua1).style.display = 'block';
+    document.querySelector('button#bqd1').style.display = 'none';
+    document.querySelector('button#bqd2').style.display = 'block';
+}
+function quadrado2(qua2){
+    document.getElementById(qua2).style.display = 'block';
+    document.querySelector('button#bqd2').style.display = 'none';
+    document.querySelector('button#bqd3').style.display = 'block';
+}
+function quadrado3(qua3){
+    document.getElementById(qua3).style.display = 'block';
+    document.querySelector('button#bqd3').style.display = 'none';
+}
+//Botões da sessão Oval
+function oval1(oval1){
+    document.getElementById(oval1).style.display = 'block';
+    document.querySelector('button#bov1').style.display = 'none';
+    document.querySelector('button#bov2').style.display = 'block';
+}
+function oval2(oval2){
+    document.getElementById(oval2).style.display = 'block';
+    document.querySelector('button#bov2').style.display = 'none';
+    document.querySelector('button#bov3').style.display = 'block';
+}
+function oval3(oval3){
+    document.getElementById(oval3).style.display = 'block';
+    document.querySelector('button#bov3').style.display = 'none';
+}
+//Botões da sessão Aviador
+function aviador1(avia1){
+    document.getElementById(avia1).style.display = 'block';
+    document.querySelector('button#bav1').style.display = 'none';
+    document.querySelector('button#bav2').style.display = 'block';
+}
+function aviador2(avia2){
+    document.getElementById(avia2).style.display = 'block';
+    document.querySelector('button#bav2').style.display = 'none';
+    document.querySelector('button#bav3').style.display = 'block';
+}
+function aviador3(avia3){
+    document.getElementById(avia3).style.display = 'block';
+    document.querySelector('button#bav3').style.display = 'none';
+}
+//Botões da sessão Estojos
+function estojos1(est1){
+    document.getElementById(est1).style.display = 'block';
+    document.querySelector('button#bes1').style.display = 'none';
+    document.querySelector('button#bes2').style.display = 'block';
+}
+function estojos2(est2){
+    document.getElementById(est2).style.display = 'block';
+    document.querySelector('button#bes2').style.display = 'none';
+    document.querySelector('button#bes3').style.display = 'block';
+}
+function estojos3(est3){
+    document.getElementById(est3).style.display = 'block';
+    document.querySelector('button#bes3').style.display = 'none';
+}
+//Botões da sessão Envelopes
+function envelopes1(env1){
+    document.getElementById(env1).style.display = 'block';
+    document.querySelector('button#ben1').style.display = 'none';
+    document.querySelector('button#ben2').style.display = 'block';
+}
+function envelopes2(env2){
+    document.getElementById(env2).style.display = 'block';
+    document.querySelector('button#ben2').style.display = 'none';
+    document.querySelector('button#ben3').style.display = 'block';
+}
+function envelopes3(env3){
+    document.getElementById(env3).style.display = 'block';
+    document.querySelector('button#ben3').style.display = 'none';
+}
+//Botões da sessão Hastes
+function hastes1(has1){
+    document.getElementById(has1).style.display = 'block';
+    document.querySelector('button#bhs1').style.display = 'none';
+    document.querySelector('button#bhs2').style.display = 'block';
+}
+function hastes2(has2){
+    document.getElementById(has2).style.display = 'block';
+    document.querySelector('button#bhs2').style.display = 'none';
+    document.querySelector('button#bhs3').style.display = 'block';
+}
+function hastes3(has3){
+    document.getElementById(has3).style.display = 'block';
+    document.querySelector('button#bhs3').style.display = 'none';
+}
+//Botões da sessão Cordas e Plaquetas
+function cordas1(pla1){
+    document.getElementById(pla1).style.display = 'block';
+    document.querySelector('button#bcp1').style.display = 'none';
+    document.querySelector('button#bcp2').style.display = 'block';
+}
+function cordas2(pla2){
+    document.getElementById(pla2).style.display = 'block';
+    document.querySelector('button#bcp2').style.display = 'none';
+    document.querySelector('button#bcp3').style.display = 'block';
+}
+function cordas3(pla3){
+    document.getElementById(pla3).style.display = 'block';
+    document.querySelector('button#bcp3').style.display = 'none';
 }
---
+++ teste/carrinho.js
@@ -0,0 +1,7 @@
+function carrinho(){
+    if(modal.style.display == 'block'){
+        modal.style.display = 'none';
+    }else{
+        modal.style.display = 'block';
+    }
+}
